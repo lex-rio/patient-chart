4457d8aad0a54b25890738f14bd9a77828e75c90
alex <oleksandr.oryshchuk@globallogic.com>
2018-11-05 15:46:39 +0200

    added edit functionality button styling

--- routes/meds.js
+++ routes/meds.js
@@ -1,15 +1,21 @@
 let router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.send('respond with a resource');
+  req.db.Med.findAll({
+    attributes: ['id', 'name', 'atx', 'dosage_form', 'is_homeopathy', 'description']
+  }).then(result => {
+    res.render('meds/index', {
+      title: 'Medicines list:',
+      meds: result
+    });
+  });
 });
 
 router.get('/:id', (req, res) => {
   req.db.Med.findByPk(req.params.id).then(result => {
     res.render('meds/view', {
-      title: `visit at ${result.date}:`,
-      visit: result,
-      meds: result.meds
+      title: `${result.name} details:`,
+      med: result
     });
   });
 });
