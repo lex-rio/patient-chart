2b754539f4332e1475fdd2d9c46d3c6f70623008
alex <oleksandr.oryshchuk@globallogic.com>
2018-11-08 13:10:09 +0200

    heroku test 1

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,27 @@
 let router = require('express').Router();
 
 router.get('/', async (req, res, next) => {
-  let [visits, doctors, patients] = await Promise.all([
-    req.db.Visit.findAll({
-      include: [
-        { model: req.db.Doctor },
-        { model: req.db.Patient }
-      ]
-    }),
-    req.db.Doctor.findAll(),
-    req.db.Patient.findAll()
-  ]).catch(next);
 
-  res.render('visit/index', {
-    title: 'Visits log',
-    visits: visits,
-    doctors: doctors,
-    patients: patients
-  });
+  res.send(process.env);
+
+  //
+  // let [visits, doctors, patients] = await Promise.all([
+  //   req.db.Visit.findAll({
+  //     include: [
+  //       { model: req.db.Doctor },
+  //       { model: req.db.Patient }
+  //     ]
+  //   }),
+  //   req.db.Doctor.findAll(),
+  //   req.db.Patient.findAll()
+  // ]).catch(next);
+  //
+  // res.render('visit/index', {
+  //   title: 'Visits log',
+  //   visits: visits,
+  //   doctors: doctors,
+  //   patients: patients
+  // });
 });
 
 module.exports = router;
